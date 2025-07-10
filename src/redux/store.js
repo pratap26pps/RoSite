35a908abae992f81d6dfc378f3d3a6790e1faed5
pratap26pps <pankajpatna10321@gmail.com>
2025-07-10 17:28:56 +0530

    store modify

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import cartReducer from "./slices/cartSlice";
-import { productReducer } from "./slices/productSlice";
 import orderReducer from "./slices/orderSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
-     order: orderReducer,
-     product: productReducer,
+    order: orderReducer,
   },
 });
