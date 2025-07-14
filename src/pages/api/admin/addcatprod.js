e4bacfa66d8d4514714922c1402d966ec21abbb9
pratap26pps <pankajpatna10321@gmail.com>
2025-07-14 15:14:11 +0530

    route change

--- src/pages/api/admin/addcatprod.js
+++ src/pages/api/admin/addcatprod.js
@@ -1,6 +1,6 @@
-import dbConnect from '../../../lib/dbConnect';
-import Category from '../../../models/Category';
-import Product from '../../../models/Product';
+import dbConnect from '@/src/lib/dbConnect';
+import Category from '@/src/models/Category';
+import Product from '@/src/models/Product';
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
